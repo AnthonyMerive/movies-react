dbf862ebdc8668bab0f317e44e2ae146a8602673
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-09-12 02:35:22 -0500

    finish slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Carousel } from 'react-bootstrap';
+
+
+export default function Slider() {
+    
+    const [peliculas, setPeliculas] = useState([]);
+
+    useEffect(() => { fetch('https://api.themoviedb.org/3/discover/movie?api_key=d35c832149972307bc3cb723c5d65bf7')
+        .then(result => result.json())
+        .then(data => setPeliculas(data.results))
+
+    }, []);
+
+    const imgURL = "https://image.tmdb.org/t/p/original"
+
+    return (<>
+
+        <Carousel fade interval={3000} className="m-1 justify-content-center">
+            {peliculas.map(movie=>
+                <Carousel.Item key={movie.id}>
+                    <img height={380}
+                        className="justify-content-center d-felx w-100"
+                        src={imgURL+movie.backdrop_path}
+                        alt="imagen"
+                    />
+                    <Carousel.Caption>
+
+                        <h3 className="text-white bg-dark">{movie.title}</h3>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+            )}
+        </Carousel>
+
+    </>)
+}
